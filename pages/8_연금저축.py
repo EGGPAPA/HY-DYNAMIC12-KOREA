c18354235dada3404578520c5588af32341323f0
EGGPAPA <hyoo0825@gmail.com>
2026-08-26 17:29:56 +0900

    짧은 사이드바 메뉴명 적용

--- pages/8_연금저축.py
+++ pages/8_연금저축.py
@@ -1,8 +1,10 @@
 import streamlit as st
+from nav_labels import inject_sidebar_labels
 from pension_manager_ui import render_pension_manager_tab
 from theme_styles import inject_theme
 
 st.set_page_config(page_title="HY DYNAMIC12 · 연금저축", page_icon="🏦", layout="wide")
 inject_theme()
+inject_sidebar_labels()
 st.title("🏦 HY DYNAMIC12 · 연금저축 관리")
 render_pension_manager_tab()
